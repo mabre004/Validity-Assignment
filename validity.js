e80bf26909d779f9b152535a90e2ad31193c27ad
Michael Abreha <michaelabreha@Michaels-MacBook-Pro.local>
2020-11-10 18:49:16 -0500

    Created a package.json file and will try a new method to parse csv to json

--- validity.js
+++ validity.js
@@ -1,11 +1,37 @@
 const csv = require("csv-parser");
 
 const fs = require("fs");
-const results = [];
+let results = [];
+let duplicates = [];
 
 fs.createReadStream("normal.csv")
   .pipe(csv({}))
-  .on("data", (data) => results.push(data))
+  .on("data", (data) => {
+    results.push(data);
+
+    // for (let i = 0; i < results.length; i++) {
+    //   let email_ad = results[i].email;
+    //   for (let j = i + 1; j < results.length; j++) {
+    //     if (email_ad === results[j].email) {
+    duplicates.push(results[0]);
+    //     }
+    //   }
+    // }
+  })
   .on("end", () => {
-    console.log(results);
+    console.log(duplicates);
   });
+
+// function getDuplicates(arr) {
+//   let duplicates = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i].email === arr[j].email) {
+//         duplicates.push(arr[i]);
+//         duplicates.push(arr[j]);
+//       }
+//     }
+//   }
+// }
+// console.log(results);
+// //console.log(getDuplicates(results));
